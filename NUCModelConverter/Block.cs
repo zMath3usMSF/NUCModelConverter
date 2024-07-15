00c0510dfb10757803c83674f43921de999560bc
zMath3usMSF <zMath3usMSF@DESKTOP-92QOI4J>
2024-07-15 15:03:08 -0300

    Adicionar arquivos de projeto.

---
+++ NUCModelConverter/Block.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUCModelConverter
+{
+    internal class Block
+    {
+        int vertexType = 0;
+        bool init = false;
+        public int currentModel = 0;
+        public int modelCount = 0;
+        bool uvSkip = true;
+        bool isChildModel = false;
+        int initIndex = 0;
+        int lastMainModel = 0;
+        public List<List<float>> vertexList = new List<List<float>>();
+        public List<List<float>> vertexNormalsList = new List<List<float>>();
+        public List<List<float>> uvsList = new List<List<float>>();
+        public List<List<int>> facesIndicesList = new List<List<int>>();
+        public List<List<int>> normalsIndicesList = new List<List<int>>();
+        public List<List<int>> uvsIndicesList = new List<List<int>>();
+        public List<List<int>> teste = new List<List<int>>();
+        public void CleanList()
+        {
+            vertexList.Clear();
+            vertexNormalsList.Clear();
+            uvsList.Clear();
+            facesIndicesList.Clear();
+            normalsIndicesList.Clear();
+            uvsIndicesList.Clear();
+            uvSkip = true;
+        }
+        public void InitLists()
+        {
+            vertexList.Add(new List<float>());
+            vertexNormalsList.Add(new List<float>());
+            uvsList.Add(new List<float>());
+            facesIndicesList.Add(new List<int>());
+            normalsIndicesList.Add(new List<int>());
+            uvsIndicesList.Add(new List<int>());
+        }
+        public void ReadChunck(MemoryStream ms, byte[] buffer)
+        {
+            int initt = BitConverter.ToInt32(buffer, 0);
+            if(initt == 0x6E01C000)
+            {
+                init = true;
+            }
+            if(init == true)
+            {
+                int type = buffer[3];
+                switch (type)
+                {
+                    case 0x62: //IDCount Int8
+                        ReadListIndicesInt8(ms, buffer);
+                        break;
+                    case 0x66: //IDCount Int16
+                        ReadListIndicesInt16(ms, buffer);
+                        break;
+                    case 0x68: //UVs
+                        ReadUVsChunck(ms, buffer);
+                        break;
+                    case 0x6A: //Indexes
+                        ReadIndexes(ms, buffer);
+                        break;
+                    case 0x6C: //Vertex
+                        ReadVertexType(ms, buffer);
+                        break;
+                    case 0x6E: //Init Model
+                        InitModel(ms, buffer);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        private void InitModel(MemoryStream ms, byte[] buffer)
+        {
+            ms.Read(buffer, 0, 4);
+            if (buffer[0] == 0x4 && buffer[1] == 0x10)
+            {
+                ms.Read(buffer, 0, 4);
+                if (buffer[0] == 0x2E)
+                {
+                    lastMainModel = vertexList.Count - 1;
+                    isChildModel = false;
+                    uvSkip = true;
+                }
+            }
+        }
+        private void ReadListIndicesInt16(MemoryStream ms, byte[] buffer)
+        {
+            int unk = buffer[0];
+            for (int i = 0; i < unk; i++)
+            {
+                ms.Read(buffer, 0, 1);
+                ms.Read(buffer, 0, 1);
+            }
+        }
+        private void ReadListIndicesInt8(MemoryStream ms, byte[] buffer)
+        {
+            teste.Clear();
+            teste.Add(new List<int>());
+            int unk = buffer[0];
+            int indexesLength = buffer[2];
+            int indexesCount = 0;
+            for (long j = 0; j < indexesLength; j++)
+            {
+                ms.Read(buffer, 0, 1);
+                if(j == 0)
+                {
+                    indexesCount = buffer[0];
+                    initIndex = buffer[0];
+                }
+                teste[0].Add(buffer[0]);
+                if (j == indexesLength - 1)
+                {
+                    for (j = ms.Position; j % 4 != 0; j++)
+                    {
+                        ms.Read(buffer, 0, 1);
+                    }
+                }
+            }
+            vertexType = unk;
+        }
+        private void ReadVertexType(MemoryStream ms, byte[] buffer)
+        {
+            switch (vertexType)
+            {
+                case 0:
+                    break;
+                case 1:
+                    ReadVertexNormalsChunck(ms, buffer);
+                    break;
+                case 2:
+                    ReadVertexChunck(ms, buffer);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void ReadVertexChunck(MemoryStream ms, byte[] buffer)
+        {
+            if(uvSkip == true)
+            {
+                InitLists();
+                currentModel++;
+            }
+            byte[] vertexBuffer = new byte[0xC];
+            int vertexLength = buffer[2];
+            for (int i = 0; i < vertexLength; i++)
+            {
+                ms.Read(vertexBuffer, 0, 0xC);
+                vertexList[currentModel - 1].Insert(teste[0][i] * 3, BitConverter.ToSingle(vertexBuffer, 0));
+                vertexList[currentModel - 1].Insert(teste[0][i] * 3 + 1, BitConverter.ToSingle(vertexBuffer, 4));
+                vertexList[currentModel - 1].Insert(teste[0][i] * 3 + 2, BitConverter.ToSingle(vertexBuffer, 8));
+                ms.Seek(4, SeekOrigin.Current);
+            }
+            if(isChildModel == true)
+            {
+                for(int i = 0; i < vertexList[currentModel - 1].Count; i++)
+                {
+                    if (vertexList[currentModel - 1][i] == 999f)
+                    {
+                        vertexList[currentModel - 1][i] = vertexList[lastMainModel][i];
+                    }
+                }
+            }
+            if (vertexList[currentModel - 1].Count != 0)
+            {
+                uvSkip = false;
+            }
+        }
+        private void ReadVertexNormalsChunck(MemoryStream ms, byte[] buffer)
+        {
+            byte[] vertexBuffer = new byte[0xC];
+            int vertexLength = buffer[2];
+            for (int i = 0; i < vertexLength; i++)
+            {
+                ms.Read(vertexBuffer, 0, 0xC);
+                vertexNormalsList[currentModel - 1].Insert(0, BitConverter.ToSingle(vertexBuffer, 8));
+                vertexNormalsList[currentModel - 1].Insert(0, BitConverter.ToSingle(vertexBuffer, 4));
+                vertexNormalsList[currentModel - 1].Insert(0, BitConverter.ToSingle(vertexBuffer, 0));
+                ms.Seek(4, SeekOrigin.Current);
+            }
+        }
+        private void ReadUVsChunck(MemoryStream ms, byte[] buffer)
+        {
+            int uvsCount = buffer[2];
+            for (int i = 0; i < uvsCount; i++)
+            {
+                ms.Read(buffer, 0, buffer.Length);
+                uvsList[currentModel - 1].Add(BitConverter.ToSingle(buffer, 0));
+                ms.Read(buffer, 0, buffer.Length);
+                uvsList[currentModel - 1].Add(BitConverter.ToSingle(buffer, 0));
+                ms.Seek(4, SeekOrigin.Current);
+            }
+        }
+        private void ReadIndexes(MemoryStream ms, byte[] buffer)
+        {
+            isChildModel = true;
+            uvSkip = true;
+            int indexesCount = buffer[2];
+            indexesCount = indexesCount * 3;
+            for (int j = 0; j < indexesCount; j += 3)
+            {
+                if (j != 0)
+                {
+                    ms.Read(buffer, 0, 1);
+                    facesIndicesList[currentModel - 1].Add(buffer[0]);
+                    ms.Read(buffer, 0, 1);
+                    normalsIndicesList[currentModel - 1].Add(buffer[0]);
+                    ms.Read(buffer, 0, 1);
+                    uvsIndicesList[currentModel - 1].Add(buffer[0]);
+                }
+                else
+                {
+                    ms.Seek(3, SeekOrigin.Current);
+                }
+            }
+            for (long k = ms.Position; k % 4 != 0; k++)
+            {
+                ms.Read(buffer, 0, 1);
+            }
+        }
+    }
+}
---
+++ NUCModelConverter/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NUCModelConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string fileName = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Raw Files (*.raw, *.sraw)|*.raw;*.sraw";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                fileName = ofd.FileName;
+                ReadModel(ofd.FileName);
+            }
+        }
+
+        private void ReadModel(string fileName)
+        {
+            using (MemoryStream ms = new MemoryStream(File.ReadAllBytes(fileName)))
+            {
+                ms.Seek(4, SeekOrigin.Begin);
+                Block block = new Block();
+                byte[] buffer = new byte[0x4];
+                block.CleanList();
+
+                ms.Read(buffer, 0, 4);
+                block.modelCount = BitConverter.ToInt32(buffer, 0);
+                block.InitLists();
+
+                for (int i = 0; i < ms.Length; i+=4)
+                {
+                    ms.Read(buffer, 0, buffer.Length);
+                    block.ReadChunck(ms, buffer);
+                }
+                int f = 0;
+                for (int i = 1; i < block.vertexList.Count; i++)
+                {
+                    f += block.vertexList[i - 1].Count / 3;
+                    for(int j = 0; j < block.facesIndicesList[i].Count; j++)
+                    {
+                        block.facesIndicesList[i][j] += f;
+                    }
+                }
+                ModelToOBJ(block.vertexList, block.vertexNormalsList, block.uvsList,
+                           block.facesIndicesList, block.normalsIndicesList, block.uvsIndicesList, block.currentModel);
+            }
+        }
+        private void ModelToOBJ(List<List<float>> vertexList, List<List<float>> normalsList, List<List<float>> uvsList, 
+                                List<List<int>> facesIndicesList, List<List<int>> normalsIndicesList, List<List<int>> uvsIndicesList,
+                                int modelCount)
+        {
+            List<List<string>> vertex = new List<List<string>>();
+            List<List<string>> uvs = new List<List<string>>();
+            List<List<string>> normals = new List<List<string>>();
+            List<List<string>> facesIndices = new List<List<string>>();
+            List<List<string>> uvsIndices = new List<List<string>>();
+            List<List<string>> normalsIndices = new List<List<string>>();
+
+            for (int i = 0; i < modelCount; i++)
+            {
+                vertex.Add(new List<string>());
+                uvs.Add(new List<string>());
+                normals.Add(new List<string>());
+                facesIndices.Add(new List<string>());
+                uvsIndices.Add(new List<string>());
+                normalsIndices.Add(new List<string>());
+            }
+            for (int i = 0; i < modelCount; i++)
+            {
+                for (int j = 0; j < vertexList[i].Count; j += 3)
+                {
+                    float x = vertexList[i][j];
+                    float y = vertexList[i][j + 1];
+                    float z = vertexList[i][j + 2];
+
+                    string formattedVertex = $"v {Math.Round(-x, 4):0.0000} {Math.Round(-y, 4):0.0000} {Math.Round(z, 4):0.0000}\n";
+                    formattedVertex = formattedVertex.Replace(',', '.');
+
+                    vertex[i].Add(formattedVertex);
+                }
+                for (int j = 0; j < uvsList[i].Count; j += 2)
+                {
+                    float x = uvsList[i][j];
+                    float y = uvsList[i][j + 1];
+
+                    string formattedUv = $"vt {Math.Round(x, 4):0.0000} {Math.Round(y, 4):0.0000}\n";
+                    formattedUv = formattedUv.Replace(',', '.');
+
+                    uvs[i].Add(formattedUv);
+                }
+                for (int j = 0; j < normalsList[i].Count; j += 3)
+                {
+                    float x = normalsList[i][j];
+                    float y = normalsList[i][j + 1];
+                    float z = normalsList[i][j + 2];
+
+                    string formattedNormal = $"vn {Math.Round(-x, 4):0.0000} {Math.Round(-y, 4):0.0000} {Math.Round(z, 4):0.0000}\n";
+                    formattedNormal = formattedNormal.Replace(',', '.');
+
+                    normals[i].Add(formattedNormal);
+                }
+                for (int j = 0; j < facesIndicesList[i].Count; j += 3)
+                {
+                    facesIndices[i].Add("f " + Convert.ToString(facesIndicesList[i][j] + 1) + " " +
+                                    Convert.ToString(facesIndicesList[i][j + 1] + 1) + " " +
+                                    Convert.ToString(facesIndicesList[i][j + 2] + 1) + "\n");
+                }
+                /*for (int j = 0; j < facesIndicesList[i].Count; j += 3)
+                {
+                    facesIndices[i].Add("f " + Convert.ToString(facesIndicesList[i][j] + 1) + "/" + Convert.ToString(uvsIndicesList[i][j] + 1) + "/" + Convert.ToString(normalsIndicesList[i][j] + 1) + " " +
+                                    Convert.ToString(facesIndicesList[i][j + 1] + 1) + "/" + Convert.ToString(uvsIndicesList[i][j + 1] + 1) + "/" + Convert.ToString(normalsIndicesList[i][j + 1] + 1) + " " +
+                                    Convert.ToString(facesIndicesList[i][j + 2] + 1) + "/" + Convert.ToString(uvsIndicesList[i][j + 2] + 1) + "/" + Convert.ToString(normalsIndicesList[i][j + 2] + 1) + "\n");
+                }*/
+            }
+            string output = "";
+            for (int i = 0; i < modelCount; i++)
+            {
+                output += string.Join("", "mtllib " + Path.GetFileNameWithoutExtension(fileName) + ".mtl" + "\n");
+                output += string.Join("", "o " + Path.GetFileNameWithoutExtension(fileName) + $"_{i}" + "\n");
+                output += string.Join("", "usemtl material1 \n\n");
+                output += string.Join("", vertex[i]) + "\n\n";
+                output += string.Join("", normals[i]) + "\n\n";
+                output += string.Join("", uvs[i]) + "\n\n";
+                output += string.Join("", facesIndices[i]) + "\n\n";
+            }
+
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(desktop, Path.GetFileNameWithoutExtension(fileName));
+            string mtlPath = Path.Combine(desktop, Path.GetFileNameWithoutExtension(fileName));
+            File.WriteAllText(filePath + ".obj", output);
+            string mtlOutput = "newmtl material1\n    map_Kd " + mtlPath + ".png";
+            File.WriteAllText(mtlPath + ".mtl", mtlOutput);
+        }
+        private void ReadIDs(MemoryStream ms, int idsCount)
+        {
+            byte[] buffer = new byte[4];
+            for (long j = 0; j < idsCount; j++)
+            {
+                ms.Read(buffer, 0, 1);
+                if (j == idsCount - 1)
+                {
+                    for (j = ms.Position; j % 4 != 0; j++)
+                    {
+                        ms.Read(buffer, 0, 1);
+                    }
+                }
+            }
+        }
+    }
+}
